dc9072714f7d9c2b0bae48efb527614d92e064c7
neverbirth@gmail.com <neverbirth@gmail.com@a96fbfb7-ef4c-5df3-07ea-496c9e2a9483>
2011-08-06 19:03:40 +0000

    Separated ImageInfo and DirectoryRecord inside their own class library. Added some more code for getting directory records.

---
+++ DotNetIsoLib/DirectoryRecord.vb
@@ -0,0 +1,93 @@
+﻿'TODO: Media flag bit?
+
+<Flags()> _
+Public Enum DirectoryRecordFlags
+    NormalFile = 0
+    Hidden = 1
+    Directory = 2
+    Associated = 4
+    NotFinalRecord = 128
+End Enum
+
+Public Class DirectoryRecord
+
+#Region " Properties "
+
+    Private _creationDate As DateTimeOffset
+    Public ReadOnly Property CreationDate() As DateTimeOffset
+        Get
+            Return _creationDate
+        End Get
+    End Property
+
+    Private _flags As DirectoryRecordFlags
+    Public ReadOnly Property Flags() As DirectoryRecordFlags
+        Get
+            Return _flags
+        End Get
+    End Property
+
+    Public ReadOnly Property IsDirectory() As Boolean
+        Get
+            Return _flags And DirectoryRecordFlags.Directory = DirectoryRecordFlags.Directory
+        End Get
+    End Property
+
+    Private _lba As Integer
+    Public ReadOnly Property LBA() As Integer
+        Get
+            Return _lba
+        End Get
+    End Property
+
+    Private _length As Long
+    Public ReadOnly Property Length() As Long
+        Get
+            Return _length
+        End Get
+    End Property
+
+    Private _name As String
+    Public ReadOnly Property Name() As String
+        Get
+            Return _name
+        End Get
+    End Property
+
+    Private _parent As DirectoryRecord
+    Public ReadOnly Property Parent() As DirectoryRecord
+        Get
+            Return _parent
+        End Get
+    End Property
+
+#End Region
+
+#Region " Constructor "
+
+    Friend Sub New(ByVal directoryRecord As ImageInfo.DirectoryRecordStruct)
+        ParseDirectoryRecordData(directoryRecord)
+    End Sub
+
+#End Region
+
+#Region " Methods "
+
+    Private Sub ParseDirectoryRecordData(ByVal value As ImageInfo.DirectoryRecordStruct)
+        _flags = value.fileFlags
+        _lba = value.lsbStart
+        _length = value.lsbDataLength
+        _name = value.fi
+
+        If Not IsDirectory AndAlso _name.Length > 2 AndAlso _name.Chars(_name.Length - 2) = ";"c Then
+            _name = _name.Substring(0, _name.Length - 2)
+        End If
+
+        _creationDate = New DateTimeOffset(1900 + value.year, value.month, value.day, _
+                                           value.hour, value.minute, value.second, _
+                                           TimeSpan.FromMinutes(value.gmtOffset * 15))
+    End Sub
+
+#End Region
+
+End Class
---
+++ DotNetIsoLib/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DotNetIsoLib")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("DotNetIsoLib")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("33492aab-2360-4aad-b117-95a206ad3e5f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
