06914d53944e0c0ef4a111ee92474e7dbd2f9be6
neverbirth@gmail.com <neverbirth@gmail.com@a96fbfb7-ef4c-5df3-07ea-496c9e2a9483>
2011-08-07 20:43:11 +0000

    Created a new usercontrol to show the Iso contents and reduce possible code inside MainForm in the future.

---
+++ trunk/CdImageMerger/Forms/ExplorerView.vb
@@ -0,0 +1,77 @@
+﻿Public Class ExplorerView
+
+#Region " Eventos "
+
+    Public Event SelectedDirectoryChanged As EventHandler
+    Public Event SelectedFileChanged As EventHandler
+
+#End Region
+
+#Region " Properties "
+
+    Private _imageInfo As DotNetIsoLib.ImageInfo
+    Public Property ImageInfo() As DotNetIsoLib.ImageInfo
+        Get
+            Return _imageInfo
+        End Get
+        Set(ByVal value As DotNetIsoLib.ImageInfo)
+            If _imageInfo IsNot value Then
+                FolderTree.Nodes.Clear()
+                FileList.Items.Clear()
+
+                _imageInfo = value
+            End If
+        End Set
+    End Property
+
+    Private _selectedDirectory As DotNetIsoLib.DirectoryRecord
+    Public ReadOnly Property SelectedDirectory() As DotNetIsoLib.DirectoryRecord
+        Get
+            Return _selectedDirectory
+        End Get
+    End Property
+
+    Private _selectedFile As DotNetIsoLib.DirectoryRecord
+    Public ReadOnly Property SelectedFile() As DotNetIsoLib.DirectoryRecord
+        Get
+            Return _selectedFile
+        End Get
+    End Property
+
+#End Region
+
+#Region " Event Handlers "
+
+    Private Sub FolderTree_AfterSelect(ByVal sender As System.Object, ByVal e As System.Windows.Forms.TreeViewEventArgs) Handles FolderTree.AfterSelect
+        OnSelectedDirectoryChanged(EventArgs.Empty)
+    End Sub
+
+    Private Sub FileList_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FileList.SelectedIndexChanged
+        OnSelectedFileChanged(EventArgs.Empty)
+    End Sub
+
+#End Region
+
+#Region " Methods "
+
+    Protected Overridable Sub OnSelectedDirectoryChanged(ByVal e As EventArgs)
+        RaiseEvent SelectedDirectoryChanged(Me, e)
+    End Sub
+
+    Protected Overridable Sub OnSelectedFileChanged(ByVal e As EventArgs)
+        RaiseEvent SelectedFileChanged(Me, e)
+    End Sub
+
+    Public Sub SetFoldersVisible(ByVal value As Boolean)
+        If Me.FolderTree.Visible AndAlso Not value Then
+            Me.FolderTree.Hide()
+            Me.SplitContainer1.Panel1Collapsed = True
+        ElseIf Not Me.FolderTree.Visible AndAlso value Then
+            Me.FolderTree.Show()
+            Me.SplitContainer1.Panel1Collapsed = False
+        End If
+    End Sub
+
+#End Region
+
+End Class
--- trunk/CdImageMerger/MainForm.Designer.vb
+++ trunk/CdImageMerger/MainForm.Designer.vb
@@ -24,19 +24,16 @@ Partial Class MainForm
     Private Sub InitializeComponent()
         Me.ImageInfoGroupBox = New System.Windows.Forms.GroupBox
         Me.ImageInfoSplitContainer = New System.Windows.Forms.SplitContainer
-        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer
-        Me.TreeView1 = New System.Windows.Forms.TreeView
-        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.SourceImageExplorerView = New CdImageMerger.ExplorerView
         Me.Label1 = New System.Windows.Forms.Label
-        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer
-        Me.TreeView2 = New System.Windows.Forms.TreeView
-        Me.ListView2 = New System.Windows.Forms.ListView
+        Me.DestinationImageExplorerView = New CdImageMerger.ExplorerView
         Me.Label2 = New System.Windows.Forms.Label
         Me.OpenBtn = New System.Windows.Forms.Button
         Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel
         Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar
         Me.StatusStrip = New System.Windows.Forms.StatusStrip
         Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.ActionCombo = New System.Windows.Forms.ComboBox
         Me.Label7 = New System.Windows.Forms.Label
         Me.Label5 = New System.Windows.Forms.Label
         Me.CdMageOffsetChk = New System.Windows.Forms.CheckBox
@@ -45,17 +42,10 @@ Partial Class MainForm
         Me.OriginStartNumeric = New System.Windows.Forms.NumericUpDown
         Me.DestinationStartNumeric = New System.Windows.Forms.NumericUpDown
         Me.OriginEndNumeric = New System.Windows.Forms.NumericUpDown
-        Me.ActionCombo = New System.Windows.Forms.ComboBox
         Me.ImageInfoGroupBox.SuspendLayout()
         Me.ImageInfoSplitContainer.Panel1.SuspendLayout()
         Me.ImageInfoSplitContainer.Panel2.SuspendLayout()
         Me.ImageInfoSplitContainer.SuspendLayout()
-        Me.SplitContainer1.Panel1.SuspendLayout()
-        Me.SplitContainer1.Panel2.SuspendLayout()
-        Me.SplitContainer1.SuspendLayout()
-        Me.SplitContainer2.Panel1.SuspendLayout()
-        Me.SplitContainer2.Panel2.SuspendLayout()
-        Me.SplitContainer2.SuspendLayout()
         Me.StatusStrip.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
         CType(Me.OriginStartNumeric, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -84,110 +74,58 @@ Partial Class MainForm
         '
         'ImageInfoSplitContainer.Panel1
         '
-        Me.ImageInfoSplitContainer.Panel1.Controls.Add(Me.SplitContainer1)
+        Me.ImageInfoSplitContainer.Panel1.Controls.Add(Me.SourceImageExplorerView)
         Me.ImageInfoSplitContainer.Panel1.Controls.Add(Me.Label1)
         '
         'ImageInfoSplitContainer.Panel2
         '
-        Me.ImageInfoSplitContainer.Panel2.Controls.Add(Me.SplitContainer2)
+        Me.ImageInfoSplitContainer.Panel2.Controls.Add(Me.DestinationImageExplorerView)
         Me.ImageInfoSplitContainer.Panel2.Controls.Add(Me.Label2)
         Me.ImageInfoSplitContainer.Size = New System.Drawing.Size(698, 311)
         Me.ImageInfoSplitContainer.SplitterDistance = 343
         Me.ImageInfoSplitContainer.SplitterWidth = 2
         Me.ImageInfoSplitContainer.TabIndex = 13
         '
-        'SplitContainer1
+        'SourceImageExplorerView
         '
-        Me.SplitContainer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+        Me.SourceImageExplorerView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
                     Or System.Windows.Forms.AnchorStyles.Left) _
                     Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.SplitContainer1.Location = New System.Drawing.Point(3, 16)
-        Me.SplitContainer1.Name = "SplitContainer1"
-        '
-        'SplitContainer1.Panel1
-        '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.TreeView1)
-        '
-        'SplitContainer1.Panel2
-        '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.ListView1)
-        Me.SplitContainer1.Size = New System.Drawing.Size(633, 505)
-        Me.SplitContainer1.SplitterDistance = 206
-        Me.SplitContainer1.SplitterWidth = 1
-        Me.SplitContainer1.TabIndex = 15
-        '
-        'TreeView1
-        '
-        Me.TreeView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TreeView1.Location = New System.Drawing.Point(0, 0)
-        Me.TreeView1.Name = "TreeView1"
-        Me.TreeView1.Size = New System.Drawing.Size(206, 505)
-        Me.TreeView1.TabIndex = 16
-        '
-        'ListView1
-        '
-        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ListView1.Location = New System.Drawing.Point(0, 0)
-        Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(426, 505)
-        Me.ListView1.TabIndex = 14
-        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.SourceImageExplorerView.ImageInfo = Nothing
+        Me.SourceImageExplorerView.Location = New System.Drawing.Point(0, 16)
+        Me.SourceImageExplorerView.Name = "SourceImageExplorerView"
+        Me.SourceImageExplorerView.Size = New System.Drawing.Size(343, 295)
+        Me.SourceImageExplorerView.TabIndex = 13
         '
         'Label1
         '
         Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(282, 0)
+        Me.Label1.Location = New System.Drawing.Point(127, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(73, 13)
         Me.Label1.TabIndex = 12
         Me.Label1.Text = "Source Image"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'SplitContainer2
+        'DestinationImageExplorerView
         '
-        Me.SplitContainer2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+        Me.DestinationImageExplorerView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
                     Or System.Windows.Forms.AnchorStyles.Left) _
                     Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.SplitContainer2.Location = New System.Drawing.Point(3, 16)
-        Me.SplitContainer2.Name = "SplitContainer2"
-        '
-        'SplitContainer2.Panel1
-        '
-        Me.SplitContainer2.Panel1.Controls.Add(Me.TreeView2)
-        '
-        'SplitContainer2.Panel2
-        '
-        Me.SplitContainer2.Panel2.Controls.Add(Me.ListView2)
-        Me.SplitContainer2.Size = New System.Drawing.Size(606, 505)
-        Me.SplitContainer2.SplitterDistance = 196
-        Me.SplitContainer2.SplitterWidth = 1
-        Me.SplitContainer2.TabIndex = 16
-        '
-        'TreeView2
-        '
-        Me.TreeView2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TreeView2.Location = New System.Drawing.Point(0, 0)
-        Me.TreeView2.Name = "TreeView2"
-        Me.TreeView2.Size = New System.Drawing.Size(196, 505)
-        Me.TreeView2.TabIndex = 16
-        '
-        'ListView2
-        '
-        Me.ListView2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ListView2.Location = New System.Drawing.Point(0, 0)
-        Me.ListView2.Name = "ListView2"
-        Me.ListView2.Size = New System.Drawing.Size(409, 505)
-        Me.ListView2.TabIndex = 14
-        Me.ListView2.UseCompatibleStateImageBehavior = False
+        Me.DestinationImageExplorerView.ImageInfo = Nothing
+        Me.DestinationImageExplorerView.Location = New System.Drawing.Point(0, 16)
+        Me.DestinationImageExplorerView.Name = "DestinationImageExplorerView"
+        Me.DestinationImageExplorerView.Size = New System.Drawing.Size(353, 295)
+        Me.DestinationImageExplorerView.TabIndex = 14
         '
         'Label2
         '
         Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(252, 0)
+        Me.Label2.Location = New System.Drawing.Point(134, 0)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(92, 13)
         Me.Label2.TabIndex = 13
@@ -245,6 +183,15 @@ Partial Class MainForm
         Me.GroupBox1.TabIndex = 8
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Action"
+        '
+        'ActionCombo
+        '
+        Me.ActionCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ActionCombo.FormattingEnabled = True
+        Me.ActionCombo.Location = New System.Drawing.Point(9, 19)
+        Me.ActionCombo.Name = "ActionCombo"
+        Me.ActionCombo.Size = New System.Drawing.Size(211, 21)
+        Me.ActionCombo.TabIndex = 8
         '
         'Label7
         '
@@ -315,15 +262,6 @@ Partial Class MainForm
         Me.OriginEndNumeric.Size = New System.Drawing.Size(108, 20)
         Me.OriginEndNumeric.TabIndex = 3
         '
-        'ActionCombo
-        '
-        Me.ActionCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ActionCombo.FormattingEnabled = True
-        Me.ActionCombo.Location = New System.Drawing.Point(9, 19)
-        Me.ActionCombo.Name = "ActionCombo"
-        Me.ActionCombo.Size = New System.Drawing.Size(211, 21)
-        Me.ActionCombo.TabIndex = 8
-        '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -341,12 +279,6 @@ Partial Class MainForm
         Me.ImageInfoSplitContainer.Panel2.ResumeLayout(False)
         Me.ImageInfoSplitContainer.Panel2.PerformLayout()
         Me.ImageInfoSplitContainer.ResumeLayout(False)
-        Me.SplitContainer1.Panel1.ResumeLayout(False)
-        Me.SplitContainer1.Panel2.ResumeLayout(False)
-        Me.SplitContainer1.ResumeLayout(False)
-        Me.SplitContainer2.Panel1.ResumeLayout(False)
-        Me.SplitContainer2.Panel2.ResumeLayout(False)
-        Me.SplitContainer2.ResumeLayout(False)
         Me.StatusStrip.ResumeLayout(False)
         Me.StatusStrip.PerformLayout()
         Me.GroupBox1.ResumeLayout(False)
@@ -375,12 +307,8 @@ Partial Class MainForm
     Friend WithEvents ImageInfoSplitContainer As System.Windows.Forms.SplitContainer
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
-    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
-    Friend WithEvents ListView1 As System.Windows.Forms.ListView
-    Friend WithEvents SplitContainer2 As System.Windows.Forms.SplitContainer
-    Friend WithEvents TreeView2 As System.Windows.Forms.TreeView
-    Friend WithEvents ListView2 As System.Windows.Forms.ListView
     Friend WithEvents ActionCombo As System.Windows.Forms.ComboBox
+    Friend WithEvents SourceImageExplorerView As CdImageMerger.ExplorerView
+    Friend WithEvents DestinationImageExplorerView As CdImageMerger.ExplorerView
 
 End Class
