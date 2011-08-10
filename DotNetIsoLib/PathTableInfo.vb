39822760511b2dd6509c49405237420a8a776331
neverbirth@gmail.com <neverbirth@gmail.com@a96fbfb7-ef4c-5df3-07ea-496c9e2a9483>
2011-08-10 23:41:55 +0000

    Path tables are now shown inside the ExplorerView control.

--- DotNetIsoLib/PathTableInfo.vb
+++ DotNetIsoLib/PathTableInfo.vb
@@ -1,5 +1,6 @@
 ﻿Public Class PathTableInfo
 
+    Private pathTableList As IList(Of PathTableEntryInfo)
 
     Private _rootDirectory As PathTableEntryInfo
     Public ReadOnly Property RootDirectory() As PathTableEntryInfo
@@ -8,8 +9,29 @@
         End Get
     End Property
 
-    Friend Sub New(ByVal rootPathElement As ImageInfo.PathTableRecord)
+    Friend Sub New()
 
+    End Sub
+
+    Public Function GetPathTableList() As IEnumerable(Of PathTableEntryInfo)
+        Return New ObjectModel.ReadOnlyCollection(Of PathTableEntryInfo)(pathTableList)
+    End Function
+
+    Friend Sub SetPathTableList(ByVal pathTableList As IList(Of ImageInfo.PathTableRecord))
+        _rootDirectory = New PathTableEntryInfo(pathTableList(0))
+
+        Dim pathTables As New List(Of PathTableEntryInfo)(pathTableList.Count)
+        pathTables.Add(_rootDirectory)
+
+        ' Can a path table entry point to a parent with an index greater than its own?
+        For i As Integer = 1 To pathTableList.Count - 1
+            Dim pathTableEntry As New PathTableEntryInfo(pathTableList(i))
+
+            pathTableEntry.SetParent(pathTables(pathTableList(i).parentDN - 1))
+            pathTables.Add(pathTableEntry)
+        Next
+
+        Me.pathTableList = pathTables
     End Sub
 
 End Class
