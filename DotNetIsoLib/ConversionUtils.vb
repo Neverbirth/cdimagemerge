195f222481726426c3d3d266cc155c6bfe2bfbb1
neverbirth@gmail.com <neverbirth@gmail.com@a96fbfb7-ef4c-5df3-07ea-496c9e2a9483>
2011-08-13 13:33:55 +0000

    Added searching of directory records.

---
+++ DotNetIsoLib/ConversionUtils.vb
@@ -0,0 +1,20 @@
+﻿Friend NotInheritable Class ConversionUtils
+
+    Public Shared Function GetRegExFromPattern(ByVal searchPattern As String) As Text.RegularExpressions.Regex
+        'Anyone feel free to add missing characters that need replacing
+        searchPattern = searchPattern.Replace(".", "\.")
+        searchPattern = searchPattern.Replace("[", "\[")
+        searchPattern = searchPattern.Replace("]", "\]")
+        searchPattern = searchPattern.Replace("$", "\$")
+        searchPattern = searchPattern.Replace("(", "\(")
+        searchPattern = searchPattern.Replace(")", "\)")
+        searchPattern = searchPattern.Replace("{", "\{")
+        searchPattern = searchPattern.Replace("}", "\}")
+        searchPattern = searchPattern.Replace("#", "\#")
+        searchPattern = searchPattern.Replace("?", ".")
+        searchPattern = searchPattern.Replace("*", ".+")
+
+        Return New Text.RegularExpressions.Regex(searchPattern, Text.RegularExpressions.RegexOptions.IgnoreCase)
+    End Function
+
+End Class
